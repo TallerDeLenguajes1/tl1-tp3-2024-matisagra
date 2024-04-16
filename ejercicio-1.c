853b62174f55607e814e26765afc0178b1accb81
fff <ggg>
2024-04-16 16:27:46 -0300

    agregar funcion de promedio por anio

--- ejercicio-1.c
+++ ejercicio-1.c
@@ -6,6 +6,7 @@
 
 void cargarGanancia(int m[][COLUMNA], int f, int c);
 void mostrarGanancia(int m[][COLUMNA], int f, int c);
+void promedioPorAnio(int m[][COLUMNA], int f, int c);
 
 int main()
 {
@@ -13,6 +14,7 @@ int main()
     int ganancia[FILA][COLUMNA];
     cargarGanancia(ganancia, FILA, COLUMNA);
     mostrarGanancia(ganancia, FILA, COLUMNA);
+    promedioPorAnio(ganancia, FILA, COLUMNA);
     return 0;
 }
 
@@ -36,4 +38,20 @@ void mostrarGanancia(int m[][COLUMNA], int f, int c)
             printf("%d\n", m[i][j]);
         }
     }
+}
+
+void promedioPorAnio(int m[][COLUMNA], int f, int c)
+{
+    int sum = 0;
+    float result;
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            sum+= m[i][j];
+        }
+        result= sum / c;
+        sum = 0;
+        printf("promedio del anio %d: %.2f\n", i+1, result);
+    }   
 }
