c64ba8e893585bd24a6852c55187ab0ec1380ae1
fff <ggg>
2024-04-16 17:03:36 -0300

    agregar funcion de minimo y maximo

--- ejercicio-1.c
+++ ejercicio-1.c
@@ -7,6 +7,7 @@
 void cargarGanancia(int m[][COLUMNA], int f, int c);
 void mostrarGanancia(int m[][COLUMNA], int f, int c);
 void promedioPorAnio(int m[][COLUMNA], int f, int c);
+void minMax(int m[][COLUMNA], int f, int c);
 
 int main()
 {
@@ -15,6 +16,7 @@ int main()
     cargarGanancia(ganancia, FILA, COLUMNA);
     mostrarGanancia(ganancia, FILA, COLUMNA);
     promedioPorAnio(ganancia, FILA, COLUMNA);
+    minMax(ganancia, FILA, COLUMNA);
     return 0;
 }
 
@@ -24,14 +26,14 @@ void cargarGanancia(int m[][COLUMNA], int f, int c)
     {
         for (int j = 0; j < c; j++)
         {
-            m[i][j] = rand() % (50000 - 10000 +1) + 10000;
+            m[i][j] = rand() % (50000 - 10000 + 1) + 10000;
         }
     }
 }
 
 void mostrarGanancia(int m[][COLUMNA], int f, int c)
 {
-     for (int i = 0; i < f; i++)
+    for (int i = 0; i < f; i++)
     {
         for (int j = 0; j < c; j++)
         {
@@ -48,10 +50,42 @@ void promedioPorAnio(int m[][COLUMNA], int f, int c)
     {
         for (int j = 0; j < c; j++)
         {
-            sum+= m[i][j];
+            sum += m[i][j];
         }
-        result= sum / c;
+        result = sum / c;
         sum = 0;
-        printf("promedio del anio %d: %.2f\n", i+1, result);
-    }   
+        printf("promedio del anio %d: %.2f\n", i + 1, result);
+    }
+}
+
+void minMax(int m[][COLUMNA], int f, int c)
+{
+    int min = 50000;
+    int max = 10000;
+    int mesMin;
+    int anioMin;
+    int mesMax;
+    int anioMax;
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (max < m[i][j])
+            {
+                max = m[i][j];
+                anioMax = i+1;
+                mesMax = j+1;
+            }
+
+            if (min > m[i][j])
+            {
+                min = m[i][j];
+                anioMin = i+1;
+                mesMin = j+1;
+            }
+        }
+    }
+
+    printf("el minimo es: %d en el anio %d en el mes %d\n", min, anioMin, mesMin);
+    printf("el maximo es: %d en el anio %d en el mes %d\n", max, anioMax, mesMax);
 }
