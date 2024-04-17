48cc6f67c6e0e6da7dd4ab2199d7fa20602469a1
Matias <sagramtias04@gmail.com>
2024-04-16 23:21:51 -0300

    ejercicio 3 terminado

--- ejercicio-3.c
+++ ejercicio-3.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 100
+#define MAX 50
 
 int main()
 {
-    char *buff;
-    char *vector;
+    char *buff[MAX];
+    char *vector[5];
 
-    for (int i = 0; i < 5; i++)
-    {
-        buff = (char *) malloc(100*sizeof(char));
-        printf("Ingrese un nombre: ");
-        gets(buff);
-
-        vector= (char *) malloc((strlen(buff)+1)*sizeof(char));
-        strcpy(vector,buff);
-
-        printf("%s",vector);
-
-        
-
-        
-
+     for (int i = 0; i < 5; i++) {
+        printf("Ingrese el nombre %d: ", i + 1);
+        scanf("%s", buff);
+        vector[i] = (char *)malloc(strlen(buff) + 1);  // Reserva de memoria
+        strcpy(vector[i], buff);  // Copiar el nombre al vector
+    }
+    
+    // Mostrar los nombres
+    printf("\nNombres ingresados:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d: %s\n", i + 1, vector[i]);
+    }
+    
+    // Liberar la memoria
+    for (int i = 0; i < 5; i++) {
+        free(vector[i]);
     }
     
     
